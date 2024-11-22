04059bacd81508e6e4860eedb3824c2da53bc774
[200~lobal user.name vitalik21-art <v.kulyhin_fit_13_24_b_d@knute.edu.ua>
2024-11-22 20:37:39 +0200

    Мій перший комміт

---
+++ Kulygin_Vitakiy.py
@@ -0,0 +1,37 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+
+print("Бульбашкове сортування:", bubble_sort(arr.copy()))
+print("Сортування вибором:", selection_sort(arr.copy()))
+print("СортуіІвання вставкою:", insertion_sort(arr.copy()))
